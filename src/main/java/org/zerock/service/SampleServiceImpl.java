504a5ab8cc5543c73f64e94da8fa96656840555d
​dino-21 <ysboo2@naver.com>
2024-04-22 14:58:34 +0900

    advice_ex

---
+++ src/main/java/org/zerock/service/SampleServiceImpl.java
@@ -0,0 +1,18 @@
+package org.zerock.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleServiceImpl implements SampleService{
+	@Override
+	public Integer doAdd(String str1, String str2) throws Exception {
+		return  Integer.parseInt(str1) + Integer.parseInt(str2);
+	}
+
+	@Override
+	public void doSample() {
+		
+		System.out.println("do Sample.......");
+		
+	}
+}
